457ad5b39f05dd26787a7e01cf4659ab64e3ae56
dayvson_menezes_local <dayvison.assis@gmail.com>
2022-03-28 09:43:09 -0300

    fix geteway

---
+++ resell-gateway/src/test/java/com/resell/gateway/ResellGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resell.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResellGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
