46c9ec071ec641a548a2797a524bd785d58837bc
dayvson_menezes_local <dayvison.assis@gmail.com>
2022-03-28 19:47:05 -0300

    start gateway

---
+++ resell-register/src/test/java/com/resell/register/ResellRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resell.register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResellRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
