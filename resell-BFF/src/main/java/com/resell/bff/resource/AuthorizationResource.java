59bead99d5b247b22391a2db992c957ab0fd1ca8
dayvson_menezes_local <dayvison.assis@gmail.com>
2022-03-28 21:38:24 -0300

    ajusts urls

--- resell-BFF/src/main/java/com/resell/bff/resource/AuthorizationResource.java
+++ resell-BFF/src/main/java/com/resell/bff/resource/AuthorizationResource.java
@@ -9,13 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "authorization")
 @RestController
-@RequestMapping("/api/resell-bff/authorization")
+@RequestMapping("/resell-bff/authorization")
 public class AuthorizationResource {
 
     @Autowired
