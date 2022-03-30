4934cf65ac714de3f65658b27bad8db4078405bc
dayvson_menezes_local <dayvison.assis@gmail.com>
2022-03-30 20:30:30 -0300

    add person

---
+++ resell-person-admin/src/test/java/com/resell/person/ResellPersonAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resell.person;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResellPersonAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
